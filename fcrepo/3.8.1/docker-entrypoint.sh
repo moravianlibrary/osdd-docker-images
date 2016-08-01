d5ba04062e8922cffa5d4468433eb710bf6d78e5
Martin Rumanek <martin@rumanek.cz>
2016-08-01 15:14:25 +0200

    fedora jdbc url

--- fcrepo/3.8.1/docker-entrypoint.sh
+++ fcrepo/3.8.1/docker-entrypoint.sh
@@ -4,9 +4,9 @@ if [ -n "$FEDORA_PASSWORD" ] ; then
   xmlstarlet ed  --inplace -u "/users/user/@password" -v $FEDORA_PASSWORD $FEDORA_HOME/server/config/fedora-users.xml 
 fi
 
-if [ -n "$FEDORA_DB_HOST" ] ; then
+if [ -n "$FEDORA_DB_JDBC_URL" ] ; then
   xmlstarlet ed -L -N x="http://www.fedora.info/definitions/1/0/config/" \
-    -u "//x:datastore[@id='localPostgreSQLPool']/x:param[@name='jdbcURL']/@value" -v "jdbc:postgresql://$FEDORA_DB_HOST/fedora3" $FEDORA_HOME/server/config/fedora.fcfg
+    -u "//x:datastore[@id='localPostgreSQLPool']/x:param[@name='jdbcURL']/@value" -v $FEDORA_DB_JDBC_URL $FEDORA_HOME/server/config/fedora.fcfg
 fi
 
 if [ -n "$FEDORA_DB_USER" ] ; then
@@ -19,9 +19,9 @@ if [ -n "$FEDORA_DB_PASSWORD" ] ; then
     -u "//x:datastore[@id='localPostgreSQLPool']/x:param[@name='dbPassword']/@value" -v $FEDORA_DB_PASSWORD $FEDORA_HOME/server/config/fedora.fcfg
 fi
 
-if [ -n "$RITRIPLES_DB_HOST" ] ; then
+if [ -n "$RITRIPLES_DB_JDBC_URL" ] ; then
   xmlstarlet ed -L -N x="http://www.fedora.info/definitions/1/0/config/" \
-    -u "//x:datastore[@id='localPostgresMPTTriplestore']/x:param[@name='jdbcURL']/@value" -v "jdbc:postgresql://$RITRIPLES_DB_HOST/riTriples" $FEDORA_HOME/server/config/fedora.fcfg
+    -u "//x:datastore[@id='localPostgresMPTTriplestore']/x:param[@name='jdbcURL']/@value" -v $RITRIPLES_DB_JDBC_URL $FEDORA_HOME/server/config/fedora.fcfg
 fi
 
 if [ -n "$RITRIPLES_DB_USER" ] ; then
