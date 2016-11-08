e6db3024bd46333a81495b81fcf8a80f91e71164
Daniel Secik <dsecik@gmail.com>
2016-11-08 15:46:57 +0100

    logs to stdout only

--- imageserver/default/imageserver/init.sh
+++
@@ -1,4 +0,0 @@
- #/bin/bash
-
-/usr/bin/spawn-fcgi -f /usr/lib/cgi-bin/iipsrv.fcgi -p $PORT -F $CHILDREN
-exec tail -F /tmp/iipsrv.log
--- imageserver/mzk/imageserver/init.sh
+++ imageserver/mzk/imageserver/init.sh
@@ -1,6 +1,4 @@
 #/bin/bash
 
 /usr/bin/spawn-fcgi -f /usr/lib/cgi-bin/iipsrv.fcgi -p $PORT -F $CHILDREN
-exec tail -F /tmp/iipsrv.log
-
-
+exec cat /tmp/iipsrv.log
