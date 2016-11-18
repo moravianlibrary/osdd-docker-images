1de43f3d48f70601ecde79126bb268cb1811e70b
Martin Rumanek <martin@rumanek.cz>
2016-11-18 09:14:55 +0100

    remove unused images

--- fcrepo/3.8/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -n "$FEDORA_PASSWORD" ] ; then
-  xmlstarlet ed  --inplace -u "/users/user/@password" -v $FEDORA_PASSWORD $FEDORA_HOME/server/config/fedora-users.xml 
-fi
-
-exec "$@"
