0124ab1904bf6922b76d0a530b7f35a690897dbb
MartinRumanek <martin@rumanek.cz>
2017-02-27 16:57:02 +0100

    s2i imageserver with optional kakadu

--- imageserver/devel/build.sh
+++ imageserver/devel/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker build -t iipsrv-builder .
-s2i build --incremental=false --rm --ref=master /home/rumanekm/workspace/iipsrv iipsrv-builder iipsrv-devel
 
+docker build -t iipsrv-builder .
+if [ -n "${KAKADU_HOME+1}" ]; then
+  s2i build --incremental=false --rm --ref=master --inject $KAKADU_HOME:/tmp/kakadu git@github.com:moravianlibrary/iipsrv.git iipsrv-builder iipsrv-devel
+else
+  s2i build --incremental=false --rm --ref=master git@github.com:moravianlibrary/iipsrv.git iipsrv-builder iipsrv-devel
+fi
